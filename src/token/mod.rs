734a5a6646dcb658f7b08804bbfec252b0945e6b
Arthur Depasse <arthur.depasse@datadoghq.com>
2022-01-29 17:38:44 +0100

    base of tokens

---
+++ src/token/mod.rs
@@ -0,0 +1,20 @@
+mod token_type;
+
+pub use token_type::TokenType;
+
+#[derive(Debug)]
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, line: usize) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            line,
+        }
+    }
+}
